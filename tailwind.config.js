b09d42f2d4c60a00eb07a8fdbd87b25c0d77efbd
Rubendepablo <rubendepablosantos@gmail.com>
2023-11-09 12:12:08 +0100

    Se agrega el proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      lato: ['Lato', 'sans-serif'], // font-lato 
+    },
+    colors: {
+      "special-gray": '#F5F0F0',
+      ...defaultTheme.colors,
+    },
+    extend: {
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['hover']
+    }
+  },
+  plugins: [],
+}
